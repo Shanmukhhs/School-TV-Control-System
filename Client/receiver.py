481bf1861b225937abcd2e480e0bff2442b22327
Shanmukh Sitturi <shanmukhsitturi@gmail.com>
2026-07-07 20:34:22 +0530

    Improve receiver with UI header and error handling

--- Client/receiver.py
+++ Client/receiver.py
@@ -1,10 +1,20 @@
-import requests,time
+import requests,time,os
 
 previous_notice=""
 while True:
-    response=requests.get("http://127.0.0.1:5000/get_notice")
-    current_notice=response.text
-    if previous_notice!=current_notice:
-        print(current_notice)
-        previous_notice=current_notice
+    try:
+        response=requests.get("http://127.0.0.1:5000/get_notice")
+        current_notice=response.text
+        if previous_notice!=current_notice:
+            os.system("cls")
+            print("==================================")
+            print(f'{"NATH VALLEY SCHOOL":^34}')
+            print("==================================\n")
+
+            print(current_notice)
+            previous_notice=current_notice
+    except Exception as e :
+        print(f"An error occured:{e}")
+
+    
     time.sleep(5)
